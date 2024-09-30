50545a8fb20d5c83553afbbb8581103f54615982
Valeria Giannelli <vg.giannellivaleria@gmail.com>
2024-09-30 14:29:07 +0200

    feat: add route api to get the technologies

--- app/Http/Controllers/Api/PageController.php
+++ app/Http/Controllers/Api/PageController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Project;
+use App\Models\Technology;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index(){
-        $projects = Project::with('technologies', 'type')->get();
+        $projects = Project::with('technologies', 'type')->paginate(10);
 
         if($projects){
             $success = true;
@@ -17,5 +18,18 @@ class PageController extends Controller
             $success = false;
         }
         return response()->json(compact('success', 'projects'));
+    }
+
+
+    public function technologies(){
+        $technologies = Technology::all();
+
+        if($technologies){
+            $success = true;
+        } else {
+            $success = false;
+        }
+
+        return response()->json(compact('success', 'technologies'));
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,5 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', [PageController::class, 'index']);
+
+Route::get('/linguaggi-di-programmazione', [PageController::class, 'technologies'] );
