5f9b88d16ac02cbdad223f81f82bf002050a29f3
Valeria Giannelli <vg.giannellivaleria@gmail.com>
2024-09-30 14:20:01 +0200

    feat: create controller and route index to get the projects

---
+++ app/Http/Controllers/Api/PageController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function index(){
+        $projects = Project::all();
+        if($projects){
+            $success = true;
+        } else {
+            $success = false;
+        }
+        return response()->json(compact('success', 'projects'));
+    }
+}
