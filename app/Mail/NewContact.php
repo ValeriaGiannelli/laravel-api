b6ccf0ecd93d085a1cbbf22d6aba695033161047
Valeria Giannelli <vg.giannellivaleria@gmail.com>
2024-10-08 13:09:15 +0200

    feat: create table and model

--- app/Mail/NewContact.php
+++ app/Mail/NewContact.php
@@ -41,7 +41,7 @@ class NewContact extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'view.name',
+            view: 'email.newcontact',
         );
     }
 
